49c411d0873e6d778751a46c37520fbfea510948
python-help-hk <[python-help-hk@example.com](mailto:python-help-hk@example.com)>
2024-03-08 02:51:49 +0800

    init commit,

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # python-help-hk.github.io
 python-help-hk.github.io
-
-
-git@github-as-python-help-hk:python-help-hk/python-help-hk.github.io.git
--- git_setup.bat
+++ git_setup.bat
@@ -1,2 +1,4 @@
+@REM git@github-as-python-help-hk:python-help-hk/python-help-hk.github.io.git
+
 git config user.name "python-help-hk"
 git config user.email "[python-help-hk@example.com](mailto:python-help-hk@example.com)"
